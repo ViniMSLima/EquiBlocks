4b7021912ce86806e07acefe7a98406a8eab7bc7
Eric Coutinho <eric.coutinhop@hotmail.com>
2024-04-22 15:24:01 -0300

    Navbar works

--- front/src/App.js
+++ front/src/App.js
@@ -1,14 +1,12 @@
 import "./App.css";
 
 import { Route, Routes } from "react-router-dom";
-import Header from "./Components/Header";
 import Home from "./Pages/Home";
 import Challenge from "./Pages/Challenge";
 
 function App() {
   return (
     <>
-      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/challenge" element={<Challenge />} />
