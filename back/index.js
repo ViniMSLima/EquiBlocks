88483767d3b85aa84ab49513339b284b41e47e6f
Emyli Quadros <babaloogames@gmail.com>
2024-04-22 16:15:17 -0300

    iniciando back, mas preciso das telas do front

--- back/index.js
+++ back/index.js
@@ -1,1 +1,23 @@
-console.log("salve pae");
+const star = 100;
+const triangle = 500;
+const square = 200;
+const circle = 700;
+const pentagon = 1000; 
+
+
+function NumSubtract()
+{
+    let numCounter = parseInt(this.dataset.numero)
+
+    if(!isNaN(numCounter)){
+        numCounter--;
+
+        this.dataset.numCounter = numCounter;
+    }
+}
+
+const components = document.querySelectorAll('????');
+
+components.forEach(componente => {
+    componente.addEventListener('click', diminuirNumero);
+});
