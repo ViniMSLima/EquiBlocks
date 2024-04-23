0869aac4fd6b410c675cead1ab2d0bf84106aad2
mateusSleite <mateusleite2709@gmail.com>
2024-04-23 14:06:53 -0300

    FormasAddContainer

--- front/src/Components/ContainerForm/index.js
+++ front/src/Components/ContainerForm/index.js
@@ -1,14 +1,26 @@
 import styles from "./styles.module.scss";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import quadrado from "../../Img/formas/square.png";
+import circulo from "../../Img/formas/circle.png";
+import triangulo from "../../Img/formas/triangulo.png";
+import pentagono from "../../Img/formas/pentagono.png";
+import estrela from "../../Img/formas/star.png";
+
+
+const formas = [quadrado, circulo, triangulo, pentagono, estrela];
 
 export default function ContainerForm() {
   return (
     <div className={styles.container}>
       <Row>
-        <Col>
-          
-        </Col>
+        {formas.map((forma, index) => (
+          <Col key={index}>
+            <div>
+              <img className={styles.forms} src={forma} alt={`Forma ${index}`} />
+            </div>
+          </Col>
+        ))}
       </Row>
     </div>
   );
