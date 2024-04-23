4b020531a9557dd606352811bbd939cbc68acf0a
mateusSleite <mateusleite2709@gmail.com>
2024-04-23 16:47:05 -0300

    BoxGOING

--- front/src/Components/Balance/index.js
+++ front/src/Components/Balance/index.js
@@ -1,24 +1,37 @@
+import React from "react";
 import styles from "./styles.module.scss";
 import balance from "../../Img/balanca3.png";
 
 export default function Balance() {
-  const handleHitBoxClick = () => {
-    console.log("Clique no hitbox!");
+  const handleHitBoxClick = (hitBoxName) => {
+    console.log(`Clique no ${hitBoxName} hitbox!`);
   };
 
   return (
     <>
-      <div
-        style={{
-          width: "2em",
-          height: "2em",
-          position: "relative",
-          top: "1.5em",
-          left: "3em",
-          backgroundColor: "blue",
-        }}
-        onClick={handleHitBoxClick}
-      >
+      <div style={{ position: "relative", display: "inline-block" }}>
+        <div
+          style={{
+            position: "absolute",
+            width: "2em",
+            height: "2em",
+            top: "1.3em",
+            left: "0.08em",
+            backgroundColor: "blue",
+          }}
+          onClick={() => handleHitBoxClick("primeiro")}
+        ></div>
+        <div
+          style={{
+            position: "absolute",
+            width: "2em",
+            height: "2em",
+            top: "1.3em",
+            left: "3.72em",
+            backgroundColor: "red",
+          }}
+          onClick={() => handleHitBoxClick("segundo")}
+        ></div>
         <img className={styles.balance} src={balance} alt="Balance" />
       </div>
     </>
