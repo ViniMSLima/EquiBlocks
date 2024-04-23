eeedc9e1eee704473f3c6655968f1063fa31d636
Eric Coutinho <eric.coutinhop@hotmail.com>
2024-04-23 16:13:50 -0300

    Removing unused imports

--- front/src/Components/Button/index.js
+++ front/src/Components/Button/index.js
@@ -1,9 +1,6 @@
 import styles from "./styles.module.scss";
-import { useNavigate } from "react-router-dom";
 
 export default function Button({ label }) {
-  const navigate = useNavigate();
-
   return (
     <button className={styles.button}>
       {label}
