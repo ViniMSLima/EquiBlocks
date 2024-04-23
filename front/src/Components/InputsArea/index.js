fcf083199a3aff38a9d224f46553d4375eb34523
Eric Coutinho <eric.coutinhop@hotmail.com>
2024-04-23 14:47:05 -0300

    Inputs cards complete

--- front/src/Components/InputsArea/index.js
+++ front/src/Components/InputsArea/index.js
@@ -1,10 +1,21 @@
+import ShapeInput from "../ShapeInput";
 import styles from "./styles.module.scss";
+
+import square from '../../Img/formas/square.png';
+import circle from '../../Img/formas/circle.png';
+import pentagon from '../../Img/formas/pentagono.png';
+import star from '../../Img/formas/star.png';
+import triangle from '../../Img/formas/triangulo.png';
 
 export default function Inputs() {
     return (
         <>
           <div className={styles.card}>
-            
+            <ShapeInput shapeImg={square} shapeValue='500'/>
+            <ShapeInput shapeImg={circle} shapeValue='?'/>
+            <ShapeInput shapeImg={pentagon} shapeValue='?'/>
+            <ShapeInput shapeImg={star} shapeValue='1000000'/>
+            <ShapeInput shapeImg={triangle} shapeValue='1000000'/>
           </div>
         </>
       );
---
+++ front/src/Components/ShapeInput/index.js
@@ -0,0 +1,20 @@
+import Form from "react-bootstrap/Form";
+
+import styles from "./styles.module.scss";
+
+export default function ShapeInput({ shapeImg, shapeValue }) {
+  return (
+    <div className={styles.bg}>
+      <div className={styles.cardBody}>
+        <img src={shapeImg} className={styles.imgFormat}></img>
+        <div className={styles.text}>
+          <Form>
+            <Form.Group controlId="Input">
+              <Form.Control type="number" placeholder="???" />
+            </Form.Group>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
+}
