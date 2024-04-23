cc7c809fb5d368d54503f49ff1a781077ec41d65
Eric Coutinho <eric.coutinhop@hotmail.com>
2024-04-23 13:58:17 -0300

    Fixing layout

---
+++ front/src/Components/InputsArea/index.js
@@ -0,0 +1,11 @@
+import styles from "./styles.module.scss";
+
+export default function Inputs() {
+    return (
+        <>
+          <div className={styles.card}>
+            
+          </div>
+        </>
+      );
+}
