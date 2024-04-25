3dc18e234170f17e544b305a598428e5264e59af
mateusSleite <mateusleite2709@gmail.com>
2024-04-25 10:44:33 -0300

    Scale Moving

--- front/src/Components/Balance/index.js
+++ front/src/Components/Balance/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "./styles.module.scss";
-import balancee from "../../Img/balanca3.png";
+import balancee from "../../Img/balanca1.png";
+import balancee2 from "../../Img/balanca2.png";
+import balancee3 from "../../Img/balanca3.png";
 import quadrado from "../../Img/formas/square.png";
 import circulo from "../../Img/formas/circle.png";
 import triangulo from "../../Img/formas/triangulo.png";
@@ -12,7 +14,7 @@ const images = {
   200: circulo,
   500: triangulo,
   700: pentagono,
-  1000: estrela
+  1000: estrela,
 };
 
 export default function Balance({ balance, balanca, handleDrop }) {
@@ -41,15 +43,59 @@ export default function Balance({ balance, balanca, handleDrop }) {
     });
   };
 
+  const determineBalanceImage = () => {
+    let balanceImage;
+    let hitboxStyles = {};
+
+    if (balance.right.total > balance.left.total) {
+      balanceImage = balancee;
+      hitboxStyles = {
+        hitbox1: { top: "0.8em", left: "0.09em" },
+        hitbox2: { top: "1.69em", left: "3.70em" },
+      };
+    } else if (balance.right.total < balance.left.total) {
+      balanceImage = balancee2;
+      hitboxStyles = {
+        hitbox1: { top: "1.7em", left: "-0.01em" },
+        hitbox2: { top: "0.75em", left: "3.80em" },
+      };
+    } else {
+      balanceImage = balancee3;
+      hitboxStyles = {
+        hitbox1: { top: "1.3em", left: "0.08em" },
+        hitbox2: { top: "1.3em", left: "3.72em" },
+      };
+    }
+
+    return { balanceImage, hitboxStyles };
+  };
+
+  const { balanceImage, hitboxStyles } = determineBalanceImage();
+
   return (
     <div style={{ position: "relative", display: "inline-block" }}>
-      <div className={styles.hitbox1} onDragOver={handleDragOver} onDrop={handleDropOnLeft}>
+      <div
+        className={styles.hitbox1}
+        style={hitboxStyles.hitbox1}
+        onDragOver={handleDragOver}
+        onDrop={handleDropOnLeft}
+      >
         {renderFigures(balance.left.figures)}
       </div>
-      <div className={styles.hitbox2} onDragOver={handleDragOver} onDrop={handleDropOnRight}>
+      <div
+        className={styles.hitbox2}
+        style={hitboxStyles.hitbox2}
+        onDragOver={handleDragOver}
+        onDrop={handleDropOnRight}
+      >
         {renderFigures(balance.right.figures)}
       </div>
-      <img className={styles.balance} src={balancee} alt="Balance" style={{ zIndex: "1" }} />
+      <img
+        className={styles.balance}
+        src={balanceImage}
+        alt="Balance"
+        style={{ zIndex: "1" }}
+      />
       <div style={{ display: "flex", flexDirection: "column" }}>
         <p>Lado A: {balance.left.total}</p>
         <p>Lado B: {balance.right.total}</p>
