8d74f039eaee193d7bcde459fb4f6cc79787dd37
ViniMSLima <vinisary.lima@gmail.com>
2024-04-26 16:49:28 -0300

    balance fixed

--- front/src/Pages/Challenge/index.js
+++ front/src/Pages/Challenge/index.js
@@ -12,6 +12,12 @@ import Inputs from "../../Components/InputsArea";
 import Balance from "../../Components/Balance";
 import ExcelGenerator from "../../Components/ExcelGenerator";
 
+import quadrado from "../../Img/formas/square.png";
+import circulo from "../../Img/formas/circle.png";
+import triangulo from "../../Img/formas/triangulo.png";
+import pentagono from "../../Img/formas/pentagono.png";
+import estrela from "../../Img/formas/star.png";
+
 import axios from "axios";
 
 import { TimerContext } from "../../Context/timerContext";
@@ -20,23 +26,32 @@ import { PesoContext } from "../../Context/pesoContext";
 export default function Challenge() {
   const [status, setStatus] = useState("Começar");
   const [phase, setPhase] = useState(localStorage.getItem("fase") || "Fase de Teste");
-  
+
   const [timerStarted, setTimerStarted] = useState(false);
   const navigate = useNavigate();
 
   const { contextTimer, setContextTimer } = useContext(TimerContext);
   const { contextPeso, setContextPeso } = useContext(PesoContext);
-  
+
   const prevPhaseRef = useRef(phase);
 
   useEffect(() => {
     localStorage.setItem("fase", phase);
+    const updatedFormas = [
+      { imagem: quadrado, quantidade: 5, peso: parseInt(contextPeso[0]), onBalance: false },
+      { imagem: circulo, quantidade: 5, peso: parseInt(contextPeso[1]), onBalance: false },
+      { imagem: triangulo, quantidade: 5, peso: parseInt(contextPeso[2]), onBalance: false },
+      { imagem: pentagono, quantidade: 5, peso: parseInt(contextPeso[3]), onBalance: false },
+      { imagem: estrela, quantidade: 5, peso: parseInt(contextPeso[4]), onBalance: false },
+    ]
+
+    localStorage.setItem("formas", JSON.stringify(updatedFormas));
   }, [phase]);
 
   useEffect(() => {
-    if(localStorage.getItem("fase") == "Desafio")
+    if (localStorage.getItem("fase") == "Desafio")
       setStatus("Finalizar");
-      setTimerStarted(true);
+    setTimerStarted(true);
   }, []);
 
   useEffect(() => {
@@ -47,100 +62,93 @@ export default function Challenge() {
       }
       return array;
     }
-  
+
     const pesos = [100, 200, 500, 700, 1000];
     const newPesos = shuffleArray(pesos);
-      
+
     setContextPeso(newPesos);
   }, []);
 
-  const [fig1, setFig1] = useState("");
-  const [fig2, setFig2] = useState("");
-  const [fig3, setFig3] = useState("");
-  const [fig4, setFig4] = useState("");
-  const [fig5, setFig5] = useState("");
+  const [fig1, setFig1] = useState(1);
+  const [fig2, setFig2] = useState(1);
+  const [fig3, setFig3] = useState(1);
+  const [fig4, setFig4] = useState(1);
+  const [fig5, setFig5] = useState(1);
 
   async function playersToMongoDB() {
+    var nome = localStorage.getItem("nome");
+    var data = localStorage.getItem("data");
+    var tempo = localStorage.getItem("tempo");
 
+    const formas1 = localStorage.getItem("formas");
+    const formas2 = JSON.parse(formas1);
+    const palpites = [fig1, fig2, fig3, fig4, fig5]
+
+    let count = 0;
+
+    formas2.forEach(element => {
+      console.log(palpites[count] + " = " + element.peso);
+      if (palpites[count] == element.peso)
+        palpites[count] = 2;
+      else
+        palpites[count] = 1;
+
+      count += 1;
+    });
+
+    // console.log(palpites);
+
+    const playerInfo = {
+      nome,
+      data,
+      tempo,
+      f1: parseInt(palpites[0]),
+      f2: parseInt(palpites[1]),
+      f3: parseInt(palpites[2]),
+      f4: parseInt(palpites[3]),
+      f5: parseInt(palpites[4]),
+    };
+
+    try {
+      //IP do senai
+      // const res = await axios.post(
+      //   "http://10.196.20.101:8080/api/postplayer",
+      //   playerInfo
+      //   );
+      const res = await axios.post(
+        "http://localhost:8080/api/postplayer",
+        playerInfo
+      );
+    } catch (error) {
+      console.error("Error fetching game data:", error);
+    }
+
+    const existingPlayersJSON = localStorage.getItem("playerInfo");
+    const existingPlayers = existingPlayersJSON
+      ? JSON.parse(existingPlayersJSON)
+      : [];
+
+    const updatedPlayers = [...existingPlayers, playerInfo];
+
+    localStorage.setItem("playerInfo", JSON.stringify(updatedPlayers));
+    setTimerStarted(false);
+    setFig1("");
+    setFig2("");
+    setFig3("");
+    setFig4("");
+    setFig5("");
   }
 
   const startReal = async () => {
     if (status === "Finalizar") {
       if (window.confirm("Deseja Finalizar?")) {
-
-        var nome = localStorage.getItem("nome");
-        var data = localStorage.getItem("data");
-        var tempo = localStorage.getItem("tempo");
-
-        const formas1 = localStorage.getItem("formas");
-
-        const formas2 = JSON.parse(formas1);
-
-        console.log(formas2);
-
-        const palpites = [fig1, fig2, fig3, fig4, fig5]
-
-        let count = 0;
-
-        formas2.forEach(element => {
-          console.log(palpites[count] + " = " + element.peso);
-          if (palpites[count] == element.peso)
-            palpites[count] = 2;
-          else
-            palpites[count] = 1;
-
-          count += 1;
-        });
-
-        console.log(palpites);
-
-        const playerInfo = {
-          nome,
-          data,
-          tempo,
-          f1: parseInt(palpites[0]),
-          f2: parseInt(palpites[1]),
-          f3: parseInt(palpites[2]),
-          f4: parseInt(palpites[3]),
-          f5: parseInt(palpites[4]),
-        };
-        
-        try {
-          //IP do senai
-          // const res = await axios.post(
-          //   "http://10.196.20.101:8080/api/postplayer",
-          //   playerInfo
-          //   );
-          const res = await axios.post(
-            "http://localhost:8080/api/postplayer",
-            playerInfo
-            );
-          } catch (error) {
-            console.error("Error fetching game data:", error);
-          }
-          
-          const existingPlayersJSON = localStorage.getItem("playerInfo");
-          const existingPlayers = existingPlayersJSON
-          ? JSON.parse(existingPlayersJSON)
-          : [];
-          
-          const updatedPlayers = [...existingPlayers, playerInfo];
-          
-          localStorage.setItem("playerInfo", JSON.stringify(updatedPlayers));
-          setTimerStarted(false);
-          setFig1("");
-          setFig2("");
-          setFig3("");
-          setFig4("");
-          setFig5("");
-
-          localStorage.clear()
-
-          navigate("/finished");
-        }
+        playersToMongoDB();
+        localStorage.clear()
+        navigate("/finished");
       }
+    }
     localStorage.setItem("balance1", JSON.stringify({ left: { total: 0, figures: {} }, right: { total: 0, figures: {} } }))
-    localStorage.setItem("balance2", JSON.stringify( { left: { total: 0, figures: {} }, right: { total: 0, figures: {} } }))
+    localStorage.setItem("balance2", JSON.stringify({ left: { total: 0, figures: {} }, right: { total: 0, figures: {} } }))
     setTimerStarted(true);
     setStatus("Finalizar");
     setPhase("Desafio");
@@ -148,8 +156,8 @@ export default function Challenge() {
 
   useEffect(() => {
     if (contextTimer > 0) {
-      setStatus("Finalizar");
-      startReal();
+      playersToMongoDB();
+      navigate("/finished");
     }
   }, [contextTimer]);
 
