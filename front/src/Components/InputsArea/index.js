548386b295946dd8465e8269e373bade2d6a50f8
ViniMSLima <vinisary.lima@gmail.com>
2024-04-27 19:11:36 -0300

    fixing button, timer and saving

--- front/src/Components/InputsArea/index.js
+++ front/src/Components/InputsArea/index.js
@@ -7,16 +7,18 @@ import pentagon from '../../Img/formas/pentagono.png';
 import star from '../../Img/formas/star.png';
 import triangle from '../../Img/formas/triangulo.png';
 
-export default function Inputs({oC1, oC2, oC3, oC4, oC5}) {
-    return (
-        <>
-          <div className={styles.card}>
-            <ShapeInput oC={oC1} shapeImg={square} shapeValue='500'/>
-            <ShapeInput oC={oC2} shapeImg={circle} shapeValue='?'/>
-            <ShapeInput oC={oC3} shapeImg={triangle} shapeValue='1000000'/>
-            <ShapeInput oC={oC4} shapeImg={pentagon} shapeValue='?'/>
-            <ShapeInput oC={oC5} shapeImg={star} shapeValue='1000000'/>
-          </div>
-        </>
-      );
+export default function Inputs({ oC1, oC2, oC3, oC4, oC5 }) {
+  return (
+    <>
+      <div className={styles.card}>
+        <ShapeInput oC={oC1} shapeImg={square} shapeValue='500' />
+        <ShapeInput oC={oC2} shapeImg={circle} shapeValue='?' />
+        <ShapeInput oC={oC3} shapeImg={triangle} shapeValue='1000000' />
+        <ShapeInput oC={oC4} shapeImg={pentagon} shapeValue='?' />
+        <ShapeInput oC={oC5} shapeImg={star} shapeValue='1000000' />
+      </div>
+
+
+    </>
+  );
 }
