ac14a87ba06760436754d88ac1050ac51cb8fb01
Eric Coutinho <eric.coutinhop@hotmail.com>
2024-05-02 14:33:16 -0300

    ******* timer working not reseting

--- front/src/Context/timerContext/index.js
+++ front/src/Context/timerContext/index.js
@@ -1,19 +1,19 @@
 import React, { useEffect, useState } from "react";
 
 export const TimerContext = React.createContext();
-TimerContext.displayName = 'Timer';
+TimerContext.displayName = "Timer";
 
 export const TimerProvider = ({ children }) => {
-    var [contextTimer, setContextTimer] = useState(0);
+  var [contextTimer, setContextTimer] = useState(0);
 
-    return (
-        <TimerContext.Provider
-            value={{
-                contextTimer,
-                setContextTimer,
-            }}
-        >
-            {children}
-        </TimerContext.Provider>
-    )
-}
+  return (
+    <TimerContext.Provider
+      value={{
+        contextTimer,
+        setContextTimer,
+      }}
+    >
+      {children}
+    </TimerContext.Provider>
+  );
+};
