9a3b11649de50817da8dbc70944d7341d3e925d6
ViniMSLima <vinisary.lima@gmail.com>
2024-05-03 16:07:18 -0300

    teste dnv kk

--- back/startup/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const player2 = require("../src/routes/player2");
-
-module.exports = function (app) 
-{
-  app
-    .use(express.json())
-    .use("/api", player2)
-};
--- front/src/api/apiEquiblocks.js
+++ front/src/api/apiEquiblocks.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export const apiEquiblocks = axios.create({
-  baseURL: "http://localhost:8080/api"
+  baseURL: "https://equiblocks-server-2.vercel.app/api"
 })
