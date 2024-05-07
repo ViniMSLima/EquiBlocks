d222714cc105ca45a6f5a5e1c9f2286863f7d4cd
Emyli Quadros <babaloogames@gmail.com>
2024-05-07 15:38:43 -0300

    creating a new interface

--- front/src/Pages/Error/index.js
+++ front/src/Pages/Error/index.js
@@ -1,6 +1,6 @@
 import Col from "react-bootstrap/esm/Col";
 import styles from "./styles.module.scss";
-
+import erro from "../../Img/404.png"
 import Container from "react-bootstrap/esm/Container";
 
 import { useNavigate } from "react-router-dom";
@@ -16,12 +16,12 @@ export default function ErrorPage() {
     <div className={styles.container}>
       <Row className={styles.body}>
           <div className={styles.border}>
-            <div className={styles.title}>ERRO 404 :(</div>
-            <div className={styles.text}>
-              A página não foi encontrada ou você não tem acesso.
-            </div>
+           <img src={erro}/>
+
           </div>
+          <div>
             <button onClick={goHome}  className={styles.button}>Voltar para a página Home</button>
+          </div>
       </Row>
     </div>
   );
